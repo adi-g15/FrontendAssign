5af3e3bca3a38e1bcfec6b6a58327f51f2db9108
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-01-05 17:17:57 +0530

    Initialised firebase; Added profile service

--- client/src/services/profiles.js
+++ client/src/services/profiles.js
@@ -1,6 +1,15 @@
 import { FETCH_ALL_USERS as FETCH_ALL_PROFILES } from "../constants/base_urls";
+// import ProfileData from "../"
 
+/**
+ * @brief - Uses the server endpoint to get the data
+ * 
+ * @returns 
+ */
 export function fetchProfiles() {
+	// return new Promise((resolve,reject) => {
+	// 	resolve();
+	// })	
 	return fetch(
 		FETCH_ALL_PROFILES,
 		{
