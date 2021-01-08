3c34c2ab37a5ed168b24bc33272692cb60f6812b
codefactor-io <support@codefactor.io>
2021-01-08 07:32:16 +0000

    [CodeFactor] Apply fixes

--- src/utils/difference.ts
+++ src/utils/difference.ts
@@ -1,9 +1,9 @@
 /**
- * 
+ *
  * @param {Array[Operations]} operationArr (const)
- * 
+ *
  * @note - Won't work in ALL cases, works for basic and current case
- * 
+ *
  * @returns copy of operations array, containing copies of previous array
  */
 function deepCopy( operationArr: operation[] ) {
@@ -18,11 +18,11 @@ function deepCopy( operationArr: operation[] ) {
 /**
  * @note - Both arguments are actually of different types, but both ideally will be having a `name` argument, that determines their uniqueness
  * 		   Also, when a operation has multiple supported con, then the operation will have more possibilities
- * 
+ *
  * @param {Array[Operations]} allOperations (const)
- * 
+ *
  * @param {Array[Filters]} usedFilters (const)
- * 
+ *
  * @returns {Array[Operations]} - Operations that can still be used
  * 								  Also the operator 'opr' in these objects are only the delta ones
  */
@@ -34,7 +34,7 @@ export function getUnusedOperations( allOperations: operation[], usedFilters: fi
 	// `type checking`... could have used typescript, but already far in the assignment, kaun dependency badhaye ab ;(
 	if(
 		!allOperations.every(operation => (operation.name && Array.isArray(operation.opr)))
-		|| !usedFilters.every(filter => (filter.name && filter.opr)) 
+		|| !usedFilters.every(filter => (filter.name && filter.opr))
 	){
 		return allOperations;
 	}
