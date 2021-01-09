4c965f3b2deae40b94e4a5d9b9ee6598d5585260
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-01-09 23:04:23 +0530

    Remove unnecessary files: \n database.rules.json offlineProfiles store.js

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import appReducers from "./reducers";   // './reducers/index.js'
-import { composeWithDevTools } from "redux-devtools-extension";
-
-// eslint-disable-next-line no-undef
-const storeEnhancers = process.env.NODE_ENV === "development" ? 
-	composeWithDevTools(): null;
-
-export default createStore(appReducers, storeEnhancers);
